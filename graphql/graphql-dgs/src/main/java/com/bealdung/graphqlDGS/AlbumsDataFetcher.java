57cda628a23235bc6dc6393c4060b03a9ebe17cf
Adrianna Zychewicz <adrianna.zychewicz@decerto.pl>
2022-01-18 14:47:50 +0100

    BAEL-5308 An Introduction to Domain Graph Service (DGS) Framework

---
+++ graphql/graphql-dgs/src/main/java/com/bealdung/graphqlDGS/AlbumsDataFetcher.java
@@ -0,0 +1,25 @@
+package com.bealdung.graphqlDGS;
+
+import com.netflix.graphql.dgs.DgsComponent;
+import com.netflix.graphql.dgs.DgsQuery;
+import com.netflix.graphql.dgs.InputArgument;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@DgsComponent
+public class AlbumsDataFetcher {
+
+    private final List<Album> albums = List.of(
+            new Album("Rumours", "Fleetwood Mac", 20),
+            new Album("What's Going On", "Marvin Gaye", 10),
+            new Album("Pet Sounds", "The Beach Boys", 12)
+    );
+
+    @DgsQuery
+    public List<Album> albums(@InputArgument String titleFilter) {
+        if(titleFilter == null) {
+            return albums;
+        }
+        return albums.stream().filter(s -> s.getTitle().contains(titleFilter)).collect(Collectors.toList());
+    }
+}
